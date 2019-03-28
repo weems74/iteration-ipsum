e362ad2c421b6185845c4942363739412fed5cac
Jason Williams <jasonwilliams@Jasons-MacBook-Pro.local>
2019-03-28 00:07:26 -0400

    fixed typo and reduced frequency of replacing words

--- incident-ipsum.py
+++ incident-ipsum.py
@@ -3,10 +3,10 @@
 
 from random import randint
 
-incident_jargon = [ "redeploying", "API", "intgration", "cloudwatch", "expired certificate", "baseline", "availability zones", "security group", "failure", "monitoring", "alert", "AWS","Azure","terraform","cloud formation script","postgres","load balancer", "endpoint", "threads", "application", "fatal", "roll back", "maintenance page", "devops", "404 error", "403 error", "500 error", "docker","container", "microservice", "object durability", "Lambda","versioning","firewall","credentials","isolating","root cause","iterating","cluster","mount","logs","feature toggle","database"]
+incident_jargon = [ "redeploying", "API", "integration", "cloudwatch", "expired certificate", "baseline", "availability zones", "security group", "failure", "monitoring", "alert", "AWS","Azure","terraform","cloud formation script","postgres","load balancer", "endpoint", "threads", "application", "fatal", "roll back", "maintenance page", "devops", "404 error", "403 error", "500 error", "docker","container", "microservice", "object durability", "Lambda","versioning","firewall","credentials","isolating","root cause","iterating","cluster","mount","logs","feature toggle","database"]
 
 def incidentize (word):
-	translate_or_not = randint(0,4)
+	translate_or_not = randint(0,5)
 	if translate_or_not > 1:
 		return word
 	else:
